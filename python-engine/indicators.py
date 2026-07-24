3fa188239e28c612248c2c08c841bd1de6332346
sohel rana <binodon777@gmail.com>
2026-07-24 09:49:39 +0600

    Refactor import statement for pandas_ta

--- python-engine/indicators.py
+++ python-engine/indicators.py
@@ -2,7 +2,7 @@
 
 import pandas as pd
 import numpy as np
-import pandas_ta as ta
+import ta
 from typing import Dict, Optional, List, Tuple
 
 # ------------------------------------------------------------
